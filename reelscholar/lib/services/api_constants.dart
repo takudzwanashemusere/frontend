4ec5c699a18eca3c9655d9d3b31e13f16ba37842
takudzwashemusere <musere2002@gmail.com>
2026-04-01 11:54:54 +0200

    api

--- reelscholar/lib/services/api_constants.dart
+++ reelscholar/lib/services/api_constants.dart
@@ -5,5 +5,6 @@
 //   const String kBaseUrl = 'http://192.168.1.5:8001';
 // iOS simulator → use localhost
 
-const String kBaseUrl = 'http://10.0.2.2:8001';
-const String kWsUrl   = 'ws://10.0.2.2:8001/ws';
+// Physical Android device on same Wi-Fi as your PC
+const String kBaseUrl = 'http://10.250.2.12:8001';
+const String kWsUrl   = 'ws://10.250.2.12:8001/ws';
